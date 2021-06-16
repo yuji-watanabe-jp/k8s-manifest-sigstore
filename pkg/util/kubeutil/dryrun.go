bbf376ae9d2eca3bf1bcc6cc6c62ccd15a851f60
hirokuni-kitahara <65647093+hirokuni-kitahara@users.noreply.github.com>
2021-06-17 01:43:44 +0900

    fix error handling in apply dry run (#5)

--- pkg/util/kubeutil/dryrun.go
+++ pkg/util/kubeutil/dryrun.go
@@ -182,7 +182,7 @@ func StrategicMergePatch(objBytes, patchBytes []byte, namespace string) ([]byte,
 	return patchedBytes, nil
 }
 
-func GetApplyPatchBytes(objBytes []byte, namespace string) ([]byte, []byte, error) {
+func GetApplyPatchBytes(manifestBytes []byte, objNamespace string) ([]byte, []byte, error) {
 	config, err := GetKubeConfig()
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error in getting k8s config; %s", err.Error())
@@ -204,32 +204,32 @@ func GetApplyPatchBytes(objBytes []byte, namespace string) ([]byte, []byte, erro
 		return nil, nil, fmt.Errorf("Failed to get OpenAPISchema; %s", err.Error())
 	}
 
-	obj := &unstructured.Unstructured{}
-	objJsonBytes, err := yaml.YAMLToJSON(objBytes)
+	mnfObj := &unstructured.Unstructured{}
+	manifestJsonBytes, err := yaml.YAMLToJSON(manifestBytes)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error in converting YamlToJson; %s", err.Error())
 	}
-	err = obj.UnmarshalJSON(objJsonBytes)
+	err = mnfObj.UnmarshalJSON(manifestJsonBytes)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error in Unmarshal into unstructured obj; %s", err.Error())
 	}
-	gvk := obj.GroupVersionKind()
+	gvk := mnfObj.GroupVersionKind()
 	gvr, _ := meta.UnsafeGuessKindToResource(gvk)
 	gvClient := dyClient.Resource(gvr)
-	claimedNamespace := obj.GetNamespace()
-	claimedName := obj.GetName()
-	if namespace != "" && claimedNamespace != "" && namespace != claimedNamespace {
-		return nil, nil, fmt.Errorf("namespace is not identical, requested: %s, defined in yaml: %s", namespace, claimedNamespace)
+	claimedNamespace := mnfObj.GetNamespace()
+	claimedName := mnfObj.GetName()
+	if objNamespace != "" && claimedNamespace != "" && objNamespace != claimedNamespace {
+		objNamespace = claimedNamespace
 	}
-	if namespace == "" && claimedNamespace != "" {
-		namespace = claimedNamespace
+	if objNamespace == "" && claimedNamespace != "" {
+		objNamespace = claimedNamespace
 	}
 
 	var currentObj *unstructured.Unstructured
-	if namespace == "" {
+	if objNamespace == "" {
 		currentObj, err = gvClient.Get(context.Background(), claimedName, metav1.GetOptions{})
 	} else {
-		currentObj, err = gvClient.Namespace(namespace).Get(context.Background(), claimedName, metav1.GetOptions{})
+		currentObj, err = gvClient.Namespace(objNamespace).Get(context.Background(), claimedName, metav1.GetOptions{})
 	}
 	if err != nil && !k8serrors.IsNotFound(err) {
 		return nil, nil, fmt.Errorf("Error in getting current obj; %s", err.Error())
@@ -243,10 +243,10 @@ func GetApplyPatchBytes(objBytes []byte, namespace string) ([]byte, []byte, erro
 	if currentObj != nil {
 		originalObjBytes, err = util.GetOriginalConfiguration(currentObj)
 		if err != nil {
-			return nil, nil, errors.Wrap(err, fmt.Sprintf("error while retrieving original configuration from:\n%v\n", obj))
+			return nil, nil, errors.Wrap(err, fmt.Sprintf("error while retrieving original configuration from:\n%v\n", mnfObj))
 		}
 	}
-	modifiedBytes, err := util.GetModifiedConfiguration(obj, true, unstructured.UnstructuredJSONScheme)
+	modifiedBytes, err := util.GetModifiedConfiguration(mnfObj, true, unstructured.UnstructuredJSONScheme)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, fmt.Sprintf("error while retrieving modified configuration from:\n%s\n", claimedName))
 	}
@@ -259,12 +259,12 @@ func GetApplyPatchBytes(objBytes []byte, namespace string) ([]byte, []byte, erro
 	createPatchErrFormat := "creating patch with:\noriginal:\n%s\nmodified:\n%s\ncurrent:\n%s\nfor:"
 	creator := scheme.Scheme
 	if !creator.Recognizes(gvk) {
-		creator.AddKnownTypeWithName(gvk, obj)
+		creator.AddKnownTypeWithName(gvk, mnfObj)
 	}
 	versionedObject, err := creator.New(gvk)
 
 	if openAPISchema != nil {
-		if schema = openAPISchema.LookupResource(obj.GroupVersionKind()); schema != nil {
+		if schema = openAPISchema.LookupResource(mnfObj.GroupVersionKind()); schema != nil {
 			lookupPatchMeta = strategicpatch.PatchMetaFromOpenAPI{Schema: schema}
 			if openapiPatch, err := strategicpatch.CreateThreeWayMergePatch(originalObjBytes, modifiedBytes, currentObjBytes, lookupPatchMeta, overwrite); err != nil {
 				fmt.Fprintf(errout, "warning: error calculating patch from openapi spec: %v\n", err)
